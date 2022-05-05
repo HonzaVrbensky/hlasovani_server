30609c1fafc3333bf887be2907727f83e87cf81d
HonzaVrbensky <91247842+HonzaVrbensky@users.noreply.github.com>
2022-05-05 11:12:09 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -4,16 +4,35 @@ radio.set_transmit_serial_number(True)
 my_serial = control.device_serial_number()
 
 name= "vote"
+name= "send"
+number= 1
 value= 0
 list_of_votes= [0,0,0,0]
+list_of_microbits = [my_serial]
 
-def voting():
+def client_working():
+    if name == "send" and number == 0:
+        radio.send_value("send", 1)
+        basic.show_number(1)
+    else:
+        radio.send_value("send", 0)
+        basic.show_number(0)
+input.on_pin_pressed(TouchPin.P0, client_working)
+
+def received_serial_number(name, value):
+    global number, list_of_microbits
+    remote_serial = radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+    if name == "learn" and value == 1:
+        if remote_serial not in list_of_microbits:
+            list_of_microbits.append(remote_serial)
+
+def voting_A():
     global list_of_votes
     radio.send_value("vote", 1)
     list_of_votes[0] +=1
     basic.show_string("A")
     print(list_of_votes)
-input.on_button_pressed(Button.A, voting)
+input.on_button_pressed(Button.A, voting_A)
 
 def voting_B():
     global list_of_votes
@@ -38,3 +57,11 @@ def voting_D():
     basic.show_string("D")
     print(list_of_votes)
 input.on_pin_pressed(TouchPin.P2, voting_D)
+
+def list_voting_append():
+    global list_of_votes
+    basic.pause(3000)
+    if name == "vote" and value == 1:
+        pass
+
+list_voting_append()
--- main.ts
+++ main.ts
@@ -3,9 +3,34 @@ radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
 let my_serial = control.deviceSerialNumber()
 let name = "vote"
+name = "send"
+let number = 1
 let value = 0
 let list_of_votes = [0, 0, 0, 0]
-input.onButtonPressed(Button.A, function voting() {
+let list_of_microbits = [my_serial]
+input.onPinPressed(TouchPin.P0, function client_working() {
+    if (name == "send" && number == 0) {
+        radio.sendValue("send", 1)
+        basic.showNumber(1)
+    } else {
+        radio.sendValue("send", 0)
+        basic.showNumber(0)
+    }
+    
+})
+function received_serial_number(name: any, value: any) {
+    
+    let remote_serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+    if (name == "learn" && value == 1) {
+        if (list_of_microbits.indexOf(remote_serial) < 0) {
+            list_of_microbits.push(remote_serial)
+        }
+        
+    }
+    
+}
+
+input.onButtonPressed(Button.A, function voting_A() {
     
     radio.sendValue("vote", 1)
     list_of_votes[0] += 1
@@ -33,3 +58,13 @@ input.onPinPressed(TouchPin.P2, function voting_D() {
     basic.showString("D")
     console.log(list_of_votes)
 })
+function list_voting_append() {
+    
+    basic.pause(3000)
+    if (name == "vote" && value == 1) {
+        
+    }
+    
+}
+
+list_voting_append()
